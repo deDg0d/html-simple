b3a92c62634c1bb6dad2ee1cfbef5cd24065a134
dani <danielramezany@yahoo.com>
2021-02-21 00:38:43 +0430

    search-box added

--- index.js
+++ index.js
@@ -4,3 +4,12 @@ function openNav(){
 function closeNav(){
   document.getElementById('myNav').style.width='0%'
 }
+function searchBox(){
+  if (document.getElementById('search-box').style.width=='25%') {
+    document.getElementById('search-box').style.width='0'
+    document.getElementById('search-box').style.visibility='hidden'
+  }else{
+  document.getElementById('search-box').style.display='block'
+  document.getElementById('search-box').style.width='25%'
+  document.getElementById('search-box').style.visibility='visible'
+}}
