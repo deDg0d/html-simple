82dce8e7183bdd26bf9b853783fef0a73780d029
dani <danielramezany@yahoo.com>
2021-02-21 22:57:23 +0430

    closing search box

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 function openNav(){
-  document.getElementById('myNav').style.width='25%'
+  document.getElementById('myNav').style.width='35%'
 }
 function closeNav(){
   document.getElementById('myNav').style.width='0%'
 }
 function searchBox(){
-  if (document.getElementById('search-box').style.width=='25%') {
+  if (document.getElementById('search-box').style.width=='35%') {
     document.getElementById('search-box').style.width='0'
     document.getElementById('search-box').style.visibility='hidden'
   }else{
   document.getElementById('search-box').style.display='block'
-  document.getElementById('search-box').style.width='25%'
+  document.getElementById('search-box').style.width='35%'
   document.getElementById('search-box').style.visibility='visible'
 }}
+function closeSearchBox(){
+  if (document.getElementById('search-box').style.width=='35%') {
+    document.getElementById('search-box').style.width='0'
+    document.getElementById('search-box').style.visibility='hidden'
+  }else{null
+}
+}
