c89789a27dfa311fb2aa1bbc1a174baea53fadd6
dani <danielramezany@yahoo.com>
2021-02-26 00:42:43 +0430

    clock and go to top button added

--- index.js
+++ index.js
@@ -20,7 +20,38 @@ function closeSearchBox(){
   }else{null
 }
 }
+window.onload =function() {time()}
+function time(){
 
-function text(){
-document.getElementById('time').innerHTML='time'
+setInterval(timer,1000)
+function timer(){
+  const date=new Date()
+  document.getElementById('time').innerHTML=date.toLocaleTimeString()
+}
+}
+
+window.onscroll=function(){
+  scrollFunction()
+}
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || 
+    document.documentElement.scrollTop > 500) {
+      document.getElementById('button-up').style.display = "block";
+  } else {
+    document.getElementById('button-up').style.display = "none";
+  }
+}
+window.onload=function(){
+  scrape()
+}
+function scrape(){
+  
+    (async()=>{
+ const req= await fetch('https://medium.com/@bretcameron/how-to-build-a-web-scraper-using-javascript-11d7cd9f77f2')
+ const res=await req.text()
+ const price=document.getElementsById('7327')
+console.log(price)
+ document.getElementById('scrape').innerHTML=price
+})()
+
 }
