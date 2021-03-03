d402d6dad814df07ac495cbc8811310e47132120
dani <danielramezany@yahoo.com>
2021-03-03 17:57:06 +0430

    scroll section addded

--- index.js
+++ index.js
@@ -20,13 +20,22 @@ function closeSearchBox(){
   }else{null
 }
 }
-window.onload =function() {time()}
+window.onload =function() {time(),scroll()}
 function time(){
+//appending child to scrolling  section 
+for (let i = 0; i<20 ; i++) { 
 
+const cursor=document.getElementById('mouse-loc') 
+//const node = document.createElement('div')
+//const textnode = document.createTextNode('')
+//node.appendChild(textnode)
+//cursor.appendChild(node)
+}
 setInterval(timer,1000)
 function timer(){
   const date=new Date()
   document.getElementById('time').innerHTML=date.toLocaleTimeString()
+  
 }
 }
 
@@ -74,4 +83,54 @@ if (value==100){
 }
 
 }
+function dom(){
+  var myJSON = {name: "John", age: 31, city: "New York"};
+  var myObj = JSON.stringify(myJSON);
+  localStorage.setItem('save',myObj)
+
+}
+function json2(){
+  const json=localStorage.getItem('save')
+  const text=JSON.parse(json)
+  document.getElementById('load').innerHTML=text.name
+}//scrolling part
+let isDown=false
+let clicked = 0
+let moveBack = false
+function mouseDown(event){
+  isDown=true 
+
+    var x = event.clientX;
+    clicked = event.clientX
+    const cursor=document.getElementById('mouse-loc')    
+      cursor.style.border='1px solid yellow'
+    }
+function mouseUp(event){
+  isDown=false
+  const cursor=document.getElementById('mouse-loc') 
+  cursor.style.border='1px solid red'
+}     
+      
+function mouseMove(event){
+  var x = event.clientX;  
+    const cursor=document.getElementById('mouse-loc')   
+     
+    return(scroll(x))
+}
+function scroll(move){
+  
+  const cursor=document.getElementById('mouse-loc') 
+  
+
+console.log('down is '+move + ' isDown is '+ isDown+' clicked pos is '+clicked)
+
+if(isDown==true){
+  const cursor=document.getElementById('mouse-loc') 
+  cursor.scrollLeft+=((clicked-move)/25)
+  
+
+  }}
+
+
+
 
